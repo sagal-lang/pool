82bd5a550ed57fb661972028b902d0f6fcb7fe57
Ali Moallim <amoallim15@gmail.com>
2024-09-13 21:59:56 +0300

    initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: (
+    <span>
+      <b>Sagal</b>
+    </span>
+  ),
+  project: {
+    link: "https://github.com/sagal/pool"
+  },
+  footer: {
+    text: <p>2024© Sagal</p>
+  }
+  // ... other theme options
+};
