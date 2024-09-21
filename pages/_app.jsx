57c14f9a98760791cd1609306fa9e23c1a30c088
Ali Moallim <amoallim15@gmail.com>
2024-09-22 02:17:53 +0300

    glossary drafted.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import "../components/assets/globals.css";
+import "../components/assets/glossary.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
